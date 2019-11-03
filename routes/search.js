56a197282efaae5495f15d822eecfbf47cb0cef6
sidx <ispichek@list.ru>
2019-11-03 19:43:33 +0300

    добавил создание таблиц базы данных

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.post('/ingredient', function(req, res, next) {
   //add db search here
-  res.render('ingredient', {title: req.body.searchVal});
+  res.render('ingredient', {title: req.body.val});
 });
 
 router.post('/recipe', function(req, res, next) {
